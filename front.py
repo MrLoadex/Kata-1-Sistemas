3f92348524f788a31be6e74f7681e381f2a5b4d1
MrLoadex <loadexx@gmail.com>
2023-06-12 17:18:35 -0300

    modificacion de nombre de variables

--- front.py
+++ front.py
@@ -32,7 +32,7 @@ class Window(tk.Tk):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
-class ListadoFrame(ttk.Frame):
+class ListedFrame(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
@@ -83,72 +83,72 @@ class ListadoFrame(ttk.Frame):
             self.listbox.insert(tk.END, "")  # Línea vacía como separador
 
 
-class AgregarFrame(ttk.Frame):
+class AddFrame(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         
         # Crear los labels y los Entry
-        self.label_apellido_nombre = ttk.Label(self, text="Apellido y Nombre:")
-        self.entry_apellido_nombre = ttk.Entry(self)
+        self.label_name_surName = ttk.Label(self, text="Apellido y Nombre:")
+        self.entry_surName_name = ttk.Entry(self)
 
         self.label_dni = ttk.Label(self, text="DNI:")
         self.entry_dni = ttk.Entry(self)
 
-        self.label_carrera = ttk.Label(self, text="Carrera:")
-        self.entry_carrera = ttk.Entry(self)
+        self.label_career = ttk.Label(self, text="Carrera:")
+        self.entry_career = ttk.Entry(self)
 
         # Crear el botón de agregar
-        self.button_agregar = ttk.Button(self, text="Agregar", command=self.agregar_estudiante, state=tk.DISABLED)
+        self.button_add = ttk.Button(self, text="Agregar", command=self.add_student, state=tk.DISABLED)
 
         # Crear el label de éxito
-        self.label_exito = ttk.Label(self, text="", foreground="green")
+        self.label_Ok = ttk.Label(self, text="", foreground="green")
 
         # Posicionar los elementos utilizando grid
-        self.label_apellido_nombre.grid(row=0, column=0, sticky="w")
-        self.entry_apellido_nombre.grid(row=1, column=0)
+        self.label_name_surName.grid(row=0, column=0, sticky="w")
+        self.entry_surName_name.grid(row=1, column=0)
 
         self.label_dni.grid(row=2, column=0, sticky="w")
         self.entry_dni.grid(row=3, column=0)
 
-        self.label_carrera.grid(row=4, column=0, sticky="w")
-        self.entry_carrera.grid(row=5, column=0)
+        self.label_career.grid(row=4, column=0, sticky="w")
+        self.entry_career.grid(row=5, column=0)
 
-        self.button_agregar.grid(row=6, column=0)
+        self.button_add.grid(row=6, column=0)
 
-        self.label_exito.grid(row=7, column=0)
+        self.label_Ok.grid(row=7, column=0)
         
         # Vincular la validación de los Entry a la actualización del estado del botón
-        self.entry_apellido_nombre.bind("<KeyRelease>", self.actualizar_estado_boton)
+        self.entry_surName_name.bind("<KeyRelease>", self.actualizar_estado_boton)
         self.entry_dni.bind("<KeyRelease>", self.actualizar_estado_boton)
-        self.entry_carrera.bind("<KeyRelease>", self.actualizar_estado_boton)
+        self.entry_career.bind("<KeyRelease>", self.actualizar_estado_boton)
         
     def actualizar_estado_boton(self, event=None):
         # Obtener los valores de los Entry
-        apellido_nombre = self.entry_apellido_nombre.get()
+        name_surName = self.entry_surName_name.get()
         dni = self.entry_dni.get()
-        carrera = self.entry_carrera.get()
+        carrera = self.entry_career.get()
 
         # Verificar si los Entry están completos
-        if apellido_nombre and dni and carrera:
-            self.button_agregar.config(state=tk.NORMAL)  # Habilitar el botón
+        if name_surName and dni and carrera:
+            self.button_add.config(state=tk.NORMAL)  # Habilitar el botón
         else:
-            self.button_agregar.config(state=tk.DISABLED) #deshabilitarlo
+            self.button_add.config(state=tk.DISABLED) #deshabilitarlo
             
-    def agregar_estudiante(self):
+    def add_student(self):
         #recupero los datos
-        name = self.entry_apellido_nombre.get()
+        name = self.entry_surName_name.get()
         DNI = self.entry_dni.get()
-        career =  self.entry_carrera.get()
+        career =  self.entry_career.get()
         
         #los inserto en la base de datos
         student_database_impl.add_student(name,DNI,career)
         
-        self.label_exito.config(text="Agregado con éxito")
+        self.label_Ok.config(text="Agregado con éxito")
         # Ocultar el label de éxito después de 2 segundos
-        self.after(1000, self.ocultar_label_exito)
+        self.after(1000, self.ocultar_label_Ok)
         
-    def ocultar_label_exito(self):
-        self.label_exito.config(text="")
+    def ocultar_label_Ok(self):
+        self.label_Ok.config(text="")
 
 
 class BuscarFrame(ttk.Frame):
@@ -160,7 +160,7 @@ class BuscarFrame(ttk.Frame):
         self.entry_id = ttk.Entry(self)
 
         # Crear el botón de buscar
-        self.button_buscar = ttk.Button(self, text="Buscar", command=self.buscar_estudiante)
+        self.button_search = ttk.Button(self, text="Buscar", command=self.search_student)
 
         # Crear la línea horizontal
         self.line = ttk.Separator(self, orient=tk.HORIZONTAL)
@@ -169,23 +169,23 @@ class BuscarFrame(ttk.Frame):
         self.label_info = ttk.Label(self, text="id: -\nnombre: -\ndni: -\ncarrera: -")
 
         # Crear el cartel "MODIFICAR"
-        self.button_modificar = ttk.Button(self, text="MODIFICAR", command= self.modificar_estudiante, state=tk.DISABLED)
+        self.button_edit = ttk.Button(self, text="MODIFICAR", command= self.student_edit, state=tk.DISABLED)
 
 
         # Crear el label de eliminar
-        self.label_eliminar = ttk.Label(self, text="Eliminar", foreground="blue", cursor="hand2")
-        self.label_eliminar.bind("<Button-1>", self.eliminar_estudiante)
+        self.label_del = ttk.Label(self, text="Eliminar", foreground="blue", cursor="hand2")
+        self.label_del.bind("<Button-1>", self.eliminar_estudiante)
 
         # Posicionar los elementos utilizando grid
         self.label_id.grid(row=0, column=0, sticky="w")
         self.entry_id.grid(row=0, column=1)
-        self.button_buscar.grid(row=0, column=2, padx=(10, 0))
+        self.button_search.grid(row=0, column=2, padx=(10, 0))
         self.line.grid(row=1, column=0, columnspan=3, sticky="we", pady=10)
         self.label_info.grid(row=2, column=0, columnspan=3, pady=10)
-        self.button_modificar.grid(row=3, column=0, columnspan=3, pady=10)
-        self.label_eliminar.grid(row=4, column=0, columnspan=3, pady=10)
+        self.button_edit.grid(row=3, column=0, columnspan=3, pady=10)
+        self.label_del.grid(row=4, column=0, columnspan=3, pady=10)
 
-    def buscar_estudiante(self):
+    def search_student(self):
         id = self.entry_id.get()
         student = student_database_impl.get_student(id)
         if student != 0:    
@@ -193,71 +193,71 @@ class BuscarFrame(ttk.Frame):
             DNI = student[1]
             career = student[2]
             self.label_info.config(text="id: " + str(id) + "\nnombre: " + name + "\ndni: " + str(DNI) + "\ncarrera: " + career)
-            self.button_modificar.config(state=tk.NORMAL)
+            self.button_edit.config(state=tk.NORMAL)
         else:
             messagebox.showerror("Error", "Estudiante no encontrado")
 
         
 
-    def modificar_estudiante(self):
+    def student_edit(self):
         id = self.entry_id.get()
-        ventana_modificar = VentanaModificar(id)
-        ventana_modificar.mainloop()
+        edit_window = EditWindow(id)
+        edit_window.mainloop()
         
     def eliminar_estudiante(self, event):
         id = self.entry_id.get()
-        respuesta = messagebox.askyesno("Advertencia", "Esta accion sera permanente y no podra deshacerse")
-        if respuesta:
+        result = messagebox.askyesno("Advertencia", "Esta accion sera permanente y no podra deshacerse")
+        if result:
             student_database_impl.del_student(id)
             messagebox.showinfo("Éxito", "El estudiante se eliminó con éxito")
 
-class VentanaModificar(tk.Toplevel):
+class EditWindow(tk.Toplevel):
     def __init__(self,id):
         super().__init__()
         self.title("Modificar")
         self.geometry("400x200")
         self.id = id
         # Crear los labels y los Entry
-        self.label_apellido_nombre = tk.Label(self, text="Apellido y Nombre:")
-        self.entry_apellido_nombre = tk.Entry(self)
+        self.label_name_surName = tk.Label(self, text="Apellido y Nombre:")
+        self.entry_surName_name = tk.Entry(self)
 
         self.label_dni = tk.Label(self, text="DNI:")
         self.entry_dni = tk.Entry(self)
 
-        self.label_carrera = tk.Label(self, text="Carrera:")
-        self.entry_carrera = tk.Entry(self)
+        self.label_career = tk.Label(self, text="Carrera:")
+        self.entry_career = tk.Entry(self)
 
         # Posicionar los elementos utilizando grid
-        self.label_apellido_nombre.grid(row=0, column=0, sticky="w")
-        self.entry_apellido_nombre.grid(row=1, column=0, padx=10)
+        self.label_name_surName.grid(row=0, column=0, sticky="w")
+        self.entry_surName_name.grid(row=1, column=0, padx=10)
 
         self.label_dni.grid(row=2, column=0, sticky="w")
         self.entry_dni.grid(row=3, column=0, padx=10)
 
-        self.label_carrera.grid(row=4, column=0, sticky="w")
-        self.entry_carrera.grid(row=5, column=0, padx=10)
+        self.label_career.grid(row=4, column=0, sticky="w")
+        self.entry_career.grid(row=5, column=0, padx=10)
 
         # Crear el frame para los botones
         self.button_frame = tk.Frame(self)
         self.button_frame.grid(row=6, column=0, pady=10)
 
         # Crear el botón Modificar
-        self.button_modificar = tk.Button(self.button_frame, text="Modificar", command=self.modificar_estudiante)
-        self.button_modificar.grid(row=0, column=0, padx=5)
+        self.button_edit = tk.Button(self.button_frame, text="Modificar", command=self.student_edit)
+        self.button_edit.grid(row=0, column=0, padx=5)
 
         # Crear el botón Cancelar
-        self.button_cancelar = tk.Button(self.button_frame, text="Cancelar", command=self.destroy)
-        self.button_cancelar.grid(row=0, column=1, padx=5)
+        self.button_cancel = tk.Button(self.button_frame, text="Cancelar", command=self.destroy)
+        self.button_cancel.grid(row=0, column=1, padx=5)
 
-    def modificar_estudiante(self):
+    def student_edit(self):
         
         #recoje los datos
-        apellido_nombre = self.entry_apellido_nombre.get()
+        name_surName = self.entry_surName_name.get()
         dni = self.entry_dni.get()
-        carrera = self.entry_carrera.get()
-        print(apellido_nombre,dni,carrera)
+        carrera = self.entry_career.get()
+        print(name_surName,dni,carrera)
         #modifica la base de datos
-        student_database_impl.update_student(DNI=dni, name= apellido_nombre, career= carrera, student_id= self.id)
+        student_database_impl.update_student(DNI=dni, name= name_surName, career= carrera, student_id= self.id)
         
         # Mostrar mensaje de éxito
         messagebox.showinfo("Éxito", "Estudiante modificado con éxito.")
@@ -273,11 +273,11 @@ student_database_impl.connect_db()
 window = Window()
 
 # Crear el frame de listado en la solapa de Listado
-listado_frame = ListadoFrame(window.flap_list)
+listado_frame = ListedFrame(window.flap_list)
 listado_frame.pack(fill=tk.BOTH, expand=True)
 
 # Crear el frame de agregar en la solapa de Agregar
-agregar_frame = AgregarFrame(window.flap_add)
+agregar_frame = AddFrame(window.flap_add)
 agregar_frame.pack(expand=True, padx=50, pady=10)
 
 # Crear el frame de buscar en la solapa de Buscar
